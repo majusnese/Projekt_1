e4c0a0a694f6750f5cdbe0c6a1141d670ae7e026
majusnese <marius.klein79@web.de>
2019-04-27 19:07:27 +0200

    Mongo scripts in lean

---
+++ Projekt/dist/utils/mongo.js
@@ -0,0 +1,29 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const shell = require("shelljs");
+const minimist = require("minimist");
+const arg = minimist(process.argv.slice(0));
+const values = arg._;
+const startserver = () => {
+    shell.exec("mongod");
+};
+const stopserver = () => {
+    shell.exec('mongo --eval "db.shutdownServer({force: true})" --norc admin');
+};
+const fillserver = () => {
+    shell.exec('mongoimport --db testdb --collection games --drop --file games.json');
+    console.log("games filled");
+    shell.exec('mongoimport --db testdb --collection sellers --drop --file sellers.json');
+    console.log("sellers filled");
+};
+switch (values[2]) {
+    case 'stop':
+        stopserver();
+        break;
+    case 'import':
+        fillserver();
+        break;
+    case 'start':
+    default:
+        startserver();
+}
