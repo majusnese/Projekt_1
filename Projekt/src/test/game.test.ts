29b0afea0d2de1d57f9f2b3e0591e6397099a178
majusnese <marius.klein79@web.de>
2019-04-27 19:16:57 +0200

    Implementieren der Tests

--- Projekt/src/test/game.test.ts
+++ Projekt/src/test/game.test.ts
@@ -1,26 +1,28 @@
 import * as mocha from "mocha";
 import * as chai from "chai";
 import chaiHttp = require("chai-http");
-import * as spiel_neu from "./spiel_neu1.json";
+import * as spiel_neu from "./spiel_neu.json";
+import * as spiel_neu_falsch from "./spiel_neu_falsch.json";
 import app from "../App";
+import { request } from "http";
 import asserArrays = require("chai-arrays");
 
 chai.use(asserArrays);
 chai.use(chaiHttp);
 const expect = chai.expect;
 
-describe("GET /games", () => {
-  it("responds with JSON and contains an array with at least one entry", async () => {
+describe("GET /games/", () => {
+  it("Alle Games", async () => {
     return chai
       .request(app)
-      .get("/games")
+      .get("/games/")
       .then(res => {
         expect(res.status).to.equal(200);
         expect(res).to.be.json;
         expect(res.body).to.have.keys("count", "games");
         let array_body = res.body.games;
         expect(array_body).to.be.array();
-        expect(array_body).to.have.lengthOf.at.least(1);
+        expect(array_body).to.have.lengthOf.at.least(2);
         let count = res.body.count;
         expect(array_body).to.have.lengthOf(count);
       });
@@ -28,7 +30,7 @@ describe("GET /games", () => {
 });
 
 describe("POST /games/", () => {
-  it("POST working as intended", () => {
+  it("Neues Game", () => {
     return chai
       .request(app)
       .post("/games/")
@@ -41,8 +43,20 @@ describe("POST /games/", () => {
   });
 });
 
+describe("POST /games/", () => {
+  it("Neues Game mit falschen Daten", () => {
+    return chai
+      .request(app)
+      .post("/games/")
+      .send(spiel_neu_falsch)
+      .then(res => {
+        expect(res.status).to.equal(500);
+      });
+  });
+});
+
 describe("DELETE /games/:id", () => {
-  it("DELETE working as intended", async () => {
+  it("Game löschen zu vorhandener ID", async () => {
     let game = await chai
       .request(app)
       .post("/games/")
@@ -61,8 +75,19 @@ describe("DELETE /games/:id", () => {
   });
 });
 
+describe("DELETE /games/:id", () => {
+  it("Versuchen nicht vorhandenes Game zu löschen", async () => {
+    return chai
+      .request(app)
+      .del("/games/" + "4cc339821b820f1f2dfdfb42")
+      .then(res => {
+        expect(res.status).to.equal(404);
+      });
+  });
+});
+
 describe("GET /games/:id", () => {
-  it("findbyid working as intended", async () => {
+  it("Game zu vorhandener ID", async () => {
     let game = await chai
       .request(app)
       .get("/games/")
@@ -83,6 +108,44 @@ describe("GET /games/:id", () => {
           "id"
         );
         expect(res.body.id).to.equal(game);
+      });
+  });
+});
+
+describe("GET /games/:name", () => {
+  it('Es existiert ein Game namens "Super Mario Bros."', async () => {
+    let game = await chai
+      .request(app)
+      .post("/games/")
+      .send(spiel_neu)
+      .then(res => {
+        return res.body.createdGame._id;
+      });
+    return chai
+      .request(app)
+      .get("/games/" + game)
+      .then(res => {
+        expect(res.status).to.equal(200);
+        expect(res).to.be.json;
+        expect(res.body).to.contain.keys(
+          "price",
+          "name",
+          "platforms",
+          "delete_request",
+          "id"
+        );
+        expect(res.body.id).to.equal(game);
+      });
+  });
+});
+
+describe("GET /games/:name", () => {
+  it('Es existiert kein Game mit "COD" im Namen', async () => {
+    chai
+      .request(app)
+      .get(`/games/?name=COD`)
+      .then(res => {
+        expect(res.status).to.equal(404);
       });
   });
 });
