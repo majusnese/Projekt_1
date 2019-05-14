2adcb88d0729233253094ae4235789a9848106b3
majusnese <marius.klein79@web.de>
2019-05-14 18:00:19 +0200

    Moving the final RC to master branch

--- Projekt/dist/test/init.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const shell = require("shelljs");
-function init() {
-    process.env.JWT_KEY = "qwertz";
-    shell.exec("npm run mongo export");
-    shell.exec("npm run mongo importtest");
-}
-exports.init = init;
